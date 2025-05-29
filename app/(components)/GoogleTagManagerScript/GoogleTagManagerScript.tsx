b7113e0d5995a361168e25ced4e8ba434bb19644
debabin <dmitriybabinup@gmail.com>
2025-05-29 23:54:02 +0700

    main 🌀 add metrics

--- app/(components)/GoogleTagManagerScript/GoogleTagManagerScript.tsx
+++ app/(components)/GoogleTagManagerScript/GoogleTagManagerScript.tsx
@@ -3,19 +3,6 @@ import Script from 'next/script';
 export const GoogleTagManagerScript = () => (
   <>
     <Script
-      id='google-tag-manager'
-      strategy='afterInteractive'
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5R7S5554');
-        `
-      }}
-    />
-    <Script
       id='google-analytics'
       src='https://www.googletagmanager.com/gtag/js?id=G-KPFPSKKZDG'
       strategy='afterInteractive'
@@ -32,13 +19,5 @@ export const GoogleTagManagerScript = () => (
         `
       }}
     />
-    <noscript>
-      <iframe
-        height='0'
-        src='https://www.googletagmanager.com/ns.html?id=GTM-5R7S5554'
-        style={{ display: 'none', visibility: 'hidden' }}
-        width='0'
-      />
-    </noscript>
   </>
 );
