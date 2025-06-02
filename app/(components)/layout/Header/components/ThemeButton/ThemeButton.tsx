840ff5b6e53848e65bd9ea70ad2237415f9d62fc
debabin <dmitriybabinup@gmail.com>
2025-06-03 01:02:59 +0700

    main 🌀 add view transition

--- app/(components)/layout/Header/components/ThemeButton/ThemeButton.tsx
+++ app/(components)/layout/Header/components/ThemeButton/ThemeButton.tsx
@@ -12,7 +12,26 @@ type ThemeButtonProps = ComponentProps<typeof Button>;
 export const ThemeButton = (props: ThemeButtonProps) => {
   const theme = useTheme();
 
-  const onThemeClick = () => theme.set(theme.value === 'dark' ? 'light' : 'dark');
+  const onThemeClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    const radius = Math.hypot(window.innerWidth, window.innerHeight);
+
+    await document.startViewTransition(() => {
+      theme.set(theme.value === 'dark' ? 'light' : 'dark');
+    }).ready;
+
+    document.documentElement.animate(
+      {
+        clipPath: [`circle(0px at ${x}px ${y}px)`, `circle(${radius}px at ${x}px ${y}px)`]
+      },
+      {
+        duration: 700,
+        easing: 'ease-in-out',
+        pseudoElement: '::view-transition-new(root)'
+      }
+    );
+  };
 
   return (
     <Button size='icon' variant='ghost' onClick={onThemeClick} {...props}>
